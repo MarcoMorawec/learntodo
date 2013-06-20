d68b858281af6ab368a0c546252c8ed0b6badbf0
marcomorawec <marco.morawec@gmail.com>
2013-06-20 12:08:04 -0400

    intial commit for a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learntodo::Application.config.secret_token = '5ac62603d1174b6eaf7ee570f764c177232f05e96e3e8857dc02d1028a4572842ff095c935a76e3765bd8db75dd198b1e21004227201ab2ebc285b8c5ad06366'
